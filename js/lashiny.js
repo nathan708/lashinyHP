7aeb7a29b59eb0cf4760146b1b2b4303dcf6cd04
横山直哉 <nathan708@hotmail.co.jp>
2023-03-22 21:00:55 +0900

    リンクスクロール　ページトップリンク実装

--- js/lashiny.js
+++ js/lashiny.js
@@ -18,3 +18,52 @@ $(window).scroll(function () {
 $(window).on('load', function () {
 	FixedAnime();/* スクロール途中からヘッダーを出現させる関数を呼ぶ*/
 });
+
+
+// ナビメニューからリンク先までスクロールさせる
+
+$('#page-link a[href*="#"]').click(function () {//全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
+	var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+	var pos = $(elmHash).offset().top-130;//idの上部の距離からHeaderの高さを引いた値を取得
+	$('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+	return false;
+});
+
+
+
+// Page top リンクを出す
+
+
+//スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+	var scroll = $(window).scrollTop();
+	if (scroll >= 200){//上から200pxスクロールしたら
+		$('#page-top').removeClass('RightMove');//#page-topについているRightMoveというクラス名を除く
+		$('#page-top').addClass('LeftMove');//#page-topについているLeftMoveというクラス名を付与
+	}else{
+		if(
+			$('#page-top').hasClass('LeftMove')){//すでに#page-topにLeftMoveというクラス名がついていたら
+			$('#page-top').removeClass('LeftMove');//LeftMoveというクラス名を除き
+			$('#page-top').addClass('RightMove');//RightMoveというクラス名を#page-topに付与
+		}
+	}
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+
+// #page-topをクリックした際の設定
+$('#page-top').click(function () {
+	$('body,html').animate({
+			scrollTop: 0//ページトップまでスクロール
+	}, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+	return false;//リンク自体の無効化
+});
